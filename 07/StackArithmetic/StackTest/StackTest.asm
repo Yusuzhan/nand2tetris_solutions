53aaf119e2a67d920d8ffc0d8e50ff5ec8b147a6
Ysz <yusuzhan@msn.com>
2018-12-13 17:56:55 +0800

    Stack test pass

--- 07/StackArithmetic/StackTest/StackTest.asm
+++ 07/StackArithmetic/StackTest/StackTest.asm
@@ -103,3 +103,291 @@ M=0
 A=M-1
 M=-1
 (LBL_NOT_EQ_2)
+// push constant 892
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+@LBL_LT_3
+D;JLT
+@SP
+A=M-1
+M=0
+@LBL_NOT_LT_3
+0;JMP
+(LBL_LT_3)
+@SP
+A=M-1
+M=-1
+(LBL_NOT_LT_3)
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 892
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+@LBL_LT_4
+D;JLT
+@SP
+A=M-1
+M=0
+@LBL_NOT_LT_4
+0;JMP
+(LBL_LT_4)
+@SP
+A=M-1
+M=-1
+(LBL_NOT_LT_4)
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+@LBL_LT_5
+D;JLT
+@SP
+A=M-1
+M=0
+@LBL_NOT_LT_5
+0;JMP
+(LBL_LT_5)
+@SP
+A=M-1
+M=-1
+(LBL_NOT_LT_5)
+// push constant 32767
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+@LBL_GT_6
+D;JGT
+@SP
+A=M-1
+M=0
+@LBL_NOT_GT_6
+0;JMP
+(LBL_GT_6)
+@SP
+A=M-1
+M=-1
+(LBL_NOT_GT_6)
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32767
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+@LBL_GT_7
+D;JGT
+@SP
+A=M-1
+M=0
+@LBL_NOT_GT_7
+0;JMP
+(LBL_GT_7)
+@SP
+A=M-1
+M=-1
+(LBL_NOT_GT_7)
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+@LBL_GT_8
+D;JGT
+@SP
+A=M-1
+M=0
+@LBL_NOT_GT_8
+0;JMP
+(LBL_GT_8)
+@SP
+A=M-1
+M=-1
+(LBL_NOT_GT_8)
+// push constant 57
+@57
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 31
+@31
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 53
+@53
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=M+D
+// push constant 112
+@112
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=M-D
+// neg
+@SP
+D=M-1
+A=D
+M=-M
+// and
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=D&M
+// push constant 82
+@82
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// or
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=D|M
+// not
+@SP
+D=M-1
+A=D
+M=!M
--- 07/vmtranslator/CodeWriter.py
+++ 07/vmtranslator/CodeWriter.py
@@ -25,6 +25,37 @@ class CodeWriter:
             self.output_file.write('D=M' + '\n')
             self.output_file.write('A=A-1' + '\n')
             self.output_file.write('M=M+D' + '\n')
+        if arg1() == 'sub':
+            self.output_file.write('@SP' + '\n')
+            self.output_file.write('M=M-1' + '\n')
+            self.output_file.write('A=M' + '\n')
+            self.output_file.write('D=M' + '\n')
+            self.output_file.write('A=A-1' + '\n')
+            self.output_file.write('M=M-D' + '\n')
+        if arg1() == 'and':
+            self.output_file.write('@SP' + '\n')
+            self.output_file.write('M=M-1' + '\n')
+            self.output_file.write('A=M' + '\n')
+            self.output_file.write('D=M' + '\n')
+            self.output_file.write('A=A-1' + '\n')
+            self.output_file.write('M=D&M' + '\n')
+        if arg1() == 'or':
+            self.output_file.write('@SP' + '\n')
+            self.output_file.write('M=M-1' + '\n')
+            self.output_file.write('A=M' + '\n')
+            self.output_file.write('D=M' + '\n')
+            self.output_file.write('A=A-1' + '\n')
+            self.output_file.write('M=D|M' + '\n')
+        if arg1() == 'neg':
+            self.output_file.write('@SP' + '\n')
+            self.output_file.write('D=M-1' + '\n')
+            self.output_file.write('A=D' + '\n')
+            self.output_file.write('M=-M' + '\n')
+        if arg1() == 'not':
+            self.output_file.write('@SP' + '\n')
+            self.output_file.write('D=M-1' + '\n')
+            self.output_file.write('A=D' + '\n')
+            self.output_file.write('M=!M' + '\n')
         elif arg1() == 'eq':
             lbl_num = self.gen_label_index()
             lbl_eq = 'LBL_EQ_' + lbl_num
@@ -48,12 +79,49 @@ class CodeWriter:
                                    + 'M=-1\n'
                                    + '(' + lbl_not_eq + ')\n')
         elif arg1() == 'lt':
+            lbl_num = self.gen_label_index()
+            lbl_lt = 'LBL_LT_' + lbl_num
+            lbl_not_lt = 'LBL_NOT_LT_' + lbl_num
             self.output_file.write('@SP' + '\n')
             self.output_file.write('M=M-1' + '\n')
             self.output_file.write('A=M' + '\n')
             self.output_file.write('D=M' + '\n')
             self.output_file.write('A=A-1' + '\n')
-            self.output_file.write('M=D-M' + '\n')
+            self.output_file.write('D=M-D\n'
+                                   + '@' + lbl_lt + '\n'
+                                   + 'D;JLT\n'
+                                   + '@SP\n'
+                                   + 'A=M-1\n'
+                                   + 'M=0\n'
+                                   + '@' + lbl_not_lt + '\n'
+                                   + '0;JMP\n'
+                                   + '(' + lbl_lt + ')\n'
+                                   + '@SP\n'
+                                   + 'A=M-1\n'
+                                   + 'M=-1\n'
+                                   + '(' + lbl_not_lt + ')\n')
+        elif arg1() == 'gt':
+            lbl_num = self.gen_label_index()
+            lbl_lt = 'LBL_GT_' + lbl_num
+            lbl_not_lt = 'LBL_NOT_GT_' + lbl_num
+            self.output_file.write('@SP' + '\n')
+            self.output_file.write('M=M-1' + '\n')
+            self.output_file.write('A=M' + '\n')
+            self.output_file.write('D=M' + '\n')
+            self.output_file.write('A=A-1' + '\n')
+            self.output_file.write('D=M-D\n'
+                                   + '@' + lbl_lt + '\n'
+                                   + 'D;JGT\n'
+                                   + '@SP\n'
+                                   + 'A=M-1\n'
+                                   + 'M=0\n'
+                                   + '@' + lbl_not_lt + '\n'
+                                   + '0;JMP\n'
+                                   + '(' + lbl_lt + ')\n'
+                                   + '@SP\n'
+                                   + 'A=M-1\n'
+                                   + 'M=-1\n'
+                                   + '(' + lbl_not_lt + ')\n')
 
     def write_push_pop(self, cmd, segment, index):
         """
