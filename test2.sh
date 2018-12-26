56920591532eabe121afd28e4eddbffd715adbc7
Ysz <yusuzhan@msn.com>
2018-12-26 18:49:11 +0800

    bug fixed/test scripts

---
+++ test2.sh
@@ -0,0 +1,5 @@
+py 10/JackCompiler/JackAnalyzer.py 10/ExpressionLessSquare
+
+TextComparer.bat "C:\workspace\others\nand2tetris_solutions\10\ExpressionLessSquare\Main_engine.xml" "C:\workspace\others\nand2tetris_solutions\10\ExpressionLessSquare\Main.xml"
+TextComparer.bat "C:\workspace\others\nand2tetris_solutions\10\ExpressionLessSquare\Square_engine.xml" "C:\workspace\others\nand2tetris_solutions\10\ExpressionLessSquare\Square.xml"
+TextComparer.bat "C:\workspace\others\nand2tetris_solutions\10\ExpressionLessSquare\SquareGame_engine.xml" "C:\workspace\others\nand2tetris_solutions\10\ExpressionLessSquare\SquareGame.xml"
--- testCompileExpressionLessSquare.sh
+++
@@ -1,5 +0,0 @@
-py 10/JackCompiler/JackAnalyzer.py 10/Square
-
-./TextComparer.bat "C:\workspace\others\nand2tetris_solutions\10\Square\Main_engine.xml" "C:\workspace\others\nand2tetris_solutions\10\Square\Main.xml"
-./TextComparer.bat "C:\workspace\others\nand2tetris_solutions\10\Square\Square_engine.xml" "C:\workspace\others\nand2tetris_solutions\10\Square\Square.xml"
-./TextComparer.bat "C:\workspace\others\nand2tetris_solutions\10\Square\SquareGame_engine.xml" "C:\workspace\others\nand2tetris_solutions\10\Square\SquareGame.xml"
