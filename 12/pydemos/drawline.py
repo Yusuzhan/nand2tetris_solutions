c96eae77f357fdd96b05b36067e5ed05a1077465
Ysz <yusuzhan@msn.com>
2019-03-21 18:42:50 +0800

    draw upwards line complete

--- 12/pydemos/drawline.py
+++ 12/pydemos/drawline.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-plt.xlim(right=100, left=0)
-plt.ylim(top=00, bottom=100)
+plt.xlim(right=200, left=0)
+plt.ylim(top=00, bottom=200)
 
 xs = []
 ys = []
@@ -23,9 +23,11 @@ def draw_line(x, y, dx, dy):
             b += 1
 
 
-def draw_line_improved(x, y, dx, dy):
+def draw_line_down(x, y, x2, y2):
     a = 0
     b = 0
+    dx = x2 - x
+    dy = y2 - y
     t = a * dy - b * dx
     while a <= dx and b <= dy:
         draw_pixel(x + a, y + b)
@@ -37,9 +39,30 @@ def draw_line_improved(x, y, dx, dy):
             t -= dx
 
 
-draw_line_improved(0, 0, 30, 20)
-draw_line_improved(30, 20, 0, 20)
+def draw_line_up(x, y, x2, y2):
+    a = 0
+    b = 0
+    dx = x2 - x
+    dy = y2 - y
+    t = a * dy - b * dx
+    while a <= dx and b >= dy:
+        draw_pixel(x + a, y + b)
+        if t > 0:
+            t += dy
+            a += 1
+        else:
+            b -= 1
+            t += dx
 
+
+# draw_line_improved(0, 0, 30, 20)
+draw_line_down(50, 50, 80, 100)
+# draw_line_improved(50, 50, 100, 80)
+# draw_line_down(50, 50, 50, 100)
+draw_line_up(0, 200, 100, 70)
+draw_line_up(0, 200, 100, 90)
+
+draw_pixel(100, 70)
 
 plt.plot(xs, ys, 's')
 plt.show()
