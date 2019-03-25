05c1040e2b3cdeb09a7a8127b1462a6c2676cafa
Yusuzhan <yusuzhan@msn.com>
2019-03-25 10:10:11 +0800

    draw circle complete

--- 12/pydemos/drawcircle.py
+++ 12/pydemos/drawcircle.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import math
 
-plt.xlim(right=30, left=0)
-plt.ylim(top=0, bottom=30)
+plt.xlim(right=256, left=0)
+plt.ylim(top=0, bottom=256)
 
 xs = []
 ys = []
@@ -13,10 +14,27 @@ def draw_pixel(x, y):
 
 
 def draw_circle(x, y, r):
+    for dy in range(- r, r + 1):
+        dx = math.sqrt(r ** 2 - dy ** 2)
+        print(dx)
+        print('drawing line from (%s, %s) to (%s, %s)' % (x - dx, y + dy, x + dx, y + dy))
+        draw_line_horizontal(x - dx, y + dy, x + dx, y + dy)
+
+
+def draw_line_horizontal(x, y, x2, y2):
+    if y < 0:
+        return
+    while x <= x2:
+        if x > 0:
+            draw_pixel(x, y)
+        x += 1
     return
 
 
-draw_circle(0, 0, 30, 20)
+# draw_circle(0, 0, 30)
+draw_circle(100, 100, 8)
+
+draw_line_horizontal(0, 0, 100, 0)
 
 plt.plot(xs, ys, 'ro')
 plt.show()
